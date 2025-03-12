57d1c6cdb884cf7d22fe385600145947e9e88e28
odinarvhage <odinarv@stud.ntnu.no>
2025-03-12 20:08:56 +0100

    feat: added function to print user information

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let name: &str = "Odin";
+    let age: u16 = 20;
+    let height: f32 = 182.2;
+    print_user_information(name,age,height);
+}
+
+fn print_user_information(name: &str, age: u16, height: f32) {
+    println!("This persons name is {}, their age is {}, and their height is {}", name, age, height);
 }
