1622ea30f0f0bd52517944926fb2c0a19ead5b86
odinarvhage <odin.arvhage@outlook.com>
2025-03-12 23:28:00 +0100

    feat: added User struct with name, position and class fix: fixed roll_dice method to return u8

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
 use rand::Rng;
 
 fn main() {
-    println!("The dice has rolled: {}!", roll_dice());
+    let mut player_one: User = User{
+        name: "Thrall".to_string(),
+        position: 0,
+        class: "Shaman".to_string()
+    };
+}
 
+struct User {
+    name: String,
+    position: u8,
+    class: String,
+}
+impl User {
+    fn read_position(&self) {
+        println!("Player is at position {}", self.position);
+    }
+    fn change_position(&mut self, input: u8) {
+        self.position += input;
+    }
+
+}
+fn roll_dice()-> u8 {
+    let mut rng = rand::rng();
+    rng.random_range(1..=6)
 }
 
 
-/**
- *
- * Returns:
- *  u8: The number rolled on the dice
- */
-fn roll_dice() -> u8 {
-    let dice_roll: u8 = rand::rng().random_range(1..=6);
-    dice_roll
-}
