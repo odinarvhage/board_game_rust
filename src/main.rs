27dc237b6bb3e067816c0faab46dfd96849cb559
odinarvhage <odin.arvhage@outlook.com>
2025-03-12 23:45:52 +0100

    feat: added basic game loop

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use rand::Rng;
 
 fn main() {
-    let mut player_one: User = User{
+    let mut player_one: User = User {
         name: "Thrall".to_string(),
         position: 0,
         class: "Shaman".to_string()
     };
+    while(player_one.position < 100) {
+        println!("The position of {} is {}.\n",player_one.name,player_one.position);
+        player_one.change_position(roll_dice())
+    }
 }
 
 struct User {
