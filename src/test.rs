d6aec8ce2d9f69f2148b4150befc4fe8828680a3
odinarvhage <odin.arvhage@outlook.com>
2025-03-19 23:45:43 +0100

    feat: added tests for the User struct

--- src/test.rs
+++ src/test.rs
@@ -3,4 +3,33 @@
 #[cfg(test)]
 mod tests {
 
+
+    #[cfg(test)]
+    mod tests {
+
+
+
+        #[test]
+        fn test_name() {
+            let mut player = crate::Player::new("Marcus".to_string(), "Car".to_string());
+            player.set_username("John".to_string());
+            assert_eq!("John", player.username);
+        }
+
+        #[test]
+        fn test_piece() {
+            let mut player = crate::Player::new("John".to_string(), "X".to_string());
+            player.set_piece("Hat".to_string());
+            assert_eq!("Hat", player.piece);
+        }
+
+        #[test]
+        fn test_position() {
+            let mut player = crate::Player::new("John".to_string(), "X".to_string());
+            player.set_position(5);
+            assert_eq!(5, player.position);
+        }
+
+
+    }
 }
